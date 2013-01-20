edec778a2bc0d5f16c1d6cc4b9c792d9d481c053
a.l.e <ale@ideale.ch>
2013-01-20 19:41:00 +0100

    dummy index.php and install.php copied from the blog-github project

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+function debug($label, $value) {
+    echo("<p>$label<br /><pre>".htmlentities(print_r($value, 1))."</pre></p>");
+}
+// phpinfo();
+// debug('server', $_SERVER);
